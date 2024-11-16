790132a7fa5f2f81adea586a2033764d23065732
ozcanakyuz <xe_bilmece@hotmail.com>
2024-11-16 20:34:01 +0300

    Ado.net DataBase
    Ado.net DataBase

---
+++ WebApp_mvc_Dbfirst/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_mvc_Dbfirst.Models;
+
+namespace WebApp_mvc_Dbfirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        urunEntities urunliste = new urunEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Liste()
+        {
+            ViewBag.Message = "ÜRÜN LİSTEMİZ";
+
+            return View(urunliste.uruntablo.ToList());
+        }
+    }
+}
---
+++ WebApp_mvc_Dbfirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp_mvc_Dbfirst.MvcApplication" Language="C#" %>
